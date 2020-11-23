6e801822ddedf9d21608986c24d30301df761c1e
Zhi Huang <huangzz@umich.edu>
2020-11-22 19:31:10 -0500

    Update from RTX TITAN

---
+++ autorun.sh
@@ -0,0 +1,22 @@
+python main.py --cohort HER2+ --seed 0; \
+python main.py --cohort HER2+ --seed 1; \
+python main.py --cohort HER2+ --seed 2; \
+python main.py --cohort HER2+ --seed 3; \
+python main.py --cohort HER2+ --seed 4; \
+python main.py --cohort HER2+ --seed 5; \
+python main.py --cohort HER2+ --seed 6; \
+python main.py --cohort HER2+ --seed 7; \
+python main.py --cohort HER2+ --seed 8; \
+python main.py --cohort HER2+ --seed 9; \
+python summary.py --cohort HER2+; \
+python main.py --cohort TNBC --seed 0; \
+python main.py --cohort TNBC --seed 1; \
+python main.py --cohort TNBC --seed 2; \
+python main.py --cohort TNBC --seed 3; \
+python main.py --cohort TNBC --seed 4; \
+python main.py --cohort TNBC --seed 5; \
+python main.py --cohort TNBC --seed 6; \
+python main.py --cohort TNBC --seed 7; \
+python main.py --cohort TNBC --seed 8; \
+python main.py --cohort TNBC --seed 9;
+python summary.py --cohort TNBC; \
